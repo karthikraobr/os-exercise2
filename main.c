da7aeca4116313cf2d736cd53b429065e208ea7d
CCS User <ccsuser@ccs-ubuntu-vm>
2016-11-10 16:34:11 +0100

    Hello world works now

--- main.c
+++ main.c
@@ -3,8 +3,16 @@ typedef unsigned short uint16_t;
 typedef unsigned char  uint8_t;
 
 int kmain(void __attribute__((__unused__)) *foo, uint32_t __attribute__((__unused__)) bar) {
+char str[]="Hello World";
+char *ptr;
+ptr=str;
+volatile char *video = (volatile char*)0xB8000;
+while(*ptr!=0){
+*video++=*ptr++;
+*video++ = 0x1B;
+//*video++=25;
+}
 
-	while(1);
 
 	return 0;
 }
